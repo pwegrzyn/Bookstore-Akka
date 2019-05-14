53d317974f70aa5328bd5b570448ec876e27bfbc
Patryk Węgrzyn <wegpat@gmail.com>
2019-05-14 19:11:19 +0200

    Initial commit

---
+++ src/main/scala/Client.scala
@@ -0,0 +1,6 @@
+
+object Client {
+  def main(args: Array[String]): Unit = {
+    println("Client init")
+  }
+}
---
+++ src/main/scala/Server.scala
@@ -0,0 +1,6 @@
+
+object Server {
+  def main(args: Array[String]): Unit = {
+    println("Server init")
+  }
+}
